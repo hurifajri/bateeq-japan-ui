cb1c621cbbfd0e033e400ee11bc927854ae2e10c
Muhamad Fajri <hurifajri@gmail.com>
2019-11-21 13:27:26 +0700

    Prettier codes

--- app/code/Moonlay/CustomHome/registration.php
+++ app/code/Moonlay/CustomHome/registration.php
@@ -1,6 +1,7 @@
 <?php
+
 \Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Moonlay_CustomHome',
-	__DIR__
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Moonlay_CustomHome',
+    __DIR__
 );
