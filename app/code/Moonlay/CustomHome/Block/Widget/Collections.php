f93651e04dbb4b9334bb6df92774b7f2e8adeb8f
Muhamad Fajri <hurifajri@gmail.com>
2019-11-22 17:07:04 +0700

    Add collections widget

--- app/code/Moonlay/CustomHome/Block/Widget/Collections.php
+++ app/code/Moonlay/CustomHome/Block/Widget/Collections.php
@@ -5,7 +5,7 @@ namespace Moonlay\CustomHome\Block\Widget;
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
 
-class Video extends Template implements BlockInterface
+class Collections extends Template implements BlockInterface
 {
-  protected $_template = "widget/video.phtml";
+  protected $_template = "widget/collections.phtml";
 }
